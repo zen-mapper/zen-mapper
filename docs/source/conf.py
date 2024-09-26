c1ffbd1fa26342c064b7f9b91e11399373ef9cf0
Ethan Rooke <ethan@roo.ke>
2024-09-25 19:44:39 -0500

    docs: enable sidebar

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,8 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "zen mapper"
+project = "Zen Mapper"
+description = "Mapper without the noise"
 copyright = "2023, Ethan Rooke"
 author = "Ethan Rooke"
 release = "0.1.4"
@@ -35,4 +36,16 @@ intersphinx_mapping = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "alabaster"
+html_theme_options = {
+        'github_user': 'zen-mapper',
+        'github_repo': 'zen-mapper',
+}
 html_static_path = ["_static"]
+html_sidebars = {
+        '**': [
+            'about.html',
+            'searchfield.html',
+            'navigation.html',
+        ]
+}
+
