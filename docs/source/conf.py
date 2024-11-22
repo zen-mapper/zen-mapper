dd2a4060c260f81e437718b31754127476b75a8f
Jacob Miller <158308043+Jamiller137@users.noreply.github.com>
2024-11-21 18:22:14 -0600

    docs: add katex functionality
    Added KaTeX functionality. Prerendering is not yet enabled. Mainly
    followed their documentation and tested macros and such inside index.md
    before committing.
    
    closes: #19
    closes: #31

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+import sphinxcontrib.katex as katex
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -22,6 +24,7 @@ extensions = [
     "myst_parser",
     "autoapi.extension",
     "sphinx_gallery.gen_gallery",
+    "sphinxcontrib.katex",
 ]
 
 templates_path = ["_templates"]
@@ -61,3 +64,27 @@ sphinx_gallery_conf = {
     "gallery_dirs": "examples",
     "filename_pattern": "/*.py",
 }
+
+# -- Options for KaTeX rendering ----------------------------------------------
+# https://sphinxcontrib-katex.readthedocs.io
+katex_css_path = "https://cdn.jsdelivr.net/npm/katex@0.16.10/dist/katex.min.css"
+katex_js_path = "katex.min.js"
+katex_autorender_path = "auto-render.min.js"
+katex_inline = [r"\(", r"\)"]
+katex_display = [r"\[", r"\]"]
+
+# Prerender requires nodeJS and KaTeX executable on PATH,
+# currently disabled for the above reason.
+katex_prerender = False
+
+# The following allows for the use of macros in HTML Sphinx builder
+# and in LaTeX based builders.
+latex_macros = r"""
+    \def \RR            #1{\mathbb{R}}
+    \def \RRn            #1{\mathbb{R}^{#1}}
+"""
+# Translates LaTeX macros to KaTeX macros/options
+katex_macros = katex.latex_defs_to_katex_macros(latex_macros)
+katex_options = "macros: {" + katex_macros + "}"
+# Adds LaTeX macros for LaTeX builder
+latex_elements = {"preamble": latex_macros}
