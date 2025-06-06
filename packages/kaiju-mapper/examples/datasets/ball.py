24a385aab24b5ff773512ec180b0cc06e56c7530
Ethan Rooke <ethan@roo.ke>
2025-06-06 15:46:21 -0500

    feat: add annulus

--- packages/kaiju-mapper/examples/datasets/ball.py
+++ packages/kaiju-mapper/examples/datasets/ball.py
@@ -1,23 +1,39 @@
 """
-Ball
-----
+Annulus
+-------
 """
 
 import matplotlib.pyplot as plt
 
-from kaiju_mapper.datasets import ball
+from kaiju_mapper.datasets import annulus
 
-data = ball(dim=2, radius=1.5, num_samples=500, seed=0xDEADBEEF)
+data = annulus(minor_radius=1, major_radius=2, num_samples=500, seed=0xDEADBEEF)
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.scatter(data[:, 0], data[:, 1])
+ax.axis("equal")
+plt.show()
+
+
+# %%
+# Like every other method this works with arbitrary dimension... it's just a
+# little tricky to showcase. Here we will hide every data point where :math:`z
+# > 0` to try and highlight what's going on.
+data = annulus(
+    dim=2,
+    minor_radius=1,
+    major_radius=2,
+    num_samples=2000,
+    seed=0xDEADBEEF,
+)
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
-ax.scatter(data[:, 0], data[:, 1], data[:, 2])
+
+mask = data[:, 2] <= 0
+ax.scatter(data[mask, 0], data[mask, 1], data[mask, 2])
 ax.axis("equal")
 plt.show()
 
 # %%
-# turns out a disk is just a one ball, look at that
-data = ball(dim=1, radius=1.5, num_samples=500, seed=0xDEADBEEF)
-plt.scatter(data[:, 0], data[:, 1])
-plt.gca().axis("equal")
-plt.show()
-
+# Well that didn't really work. Until we have interactive plots to peruse you
+# will probably just have to trust us on this one.
