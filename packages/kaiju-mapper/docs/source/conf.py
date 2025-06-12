64c5215342497c7c0dc4af6d32408ee27695bb78
Ethan Rooke <ethan@roo.ke>
2025-06-12 14:22:05 -0500

    enable auto api docs

--- packages/kaiju-mapper/docs/source/conf.py
+++ packages/kaiju-mapper/docs/source/conf.py
@@ -6,23 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'kaiju mapper'
-copyright = '2025, Ethan Rooke, Jacob Miller'
-author = 'Ethan Rooke, Jacob Miller'
-release = '0.1.1'
+project = "kaiju mapper"
+copyright = "2025, Ethan Rooke, Jacob Miller"
+author = "Ethan Rooke, Jacob Miller"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "autoapi.extension",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
+# -- Options for Api documentation --------------------------------------------
+autoapi_dirs = ["../../src/kaiju_mapper/"]
+autoapi_ignore = ["**/test*.py"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "pydata_sphinx_theme"
-html_static_path = ['_static']
+html_static_path = ["_static"]
