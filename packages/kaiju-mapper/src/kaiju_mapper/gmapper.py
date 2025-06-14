a5bf64e64e55574829164f9c7ca893dd789af204
Ethan Rooke <ethan@roo.ke>
2025-06-13 19:32:44 -0500

    add basic tests for gmapper

--- packages/kaiju-mapper/src/kaiju_mapper/gmapper.py
+++ packages/kaiju-mapper/src/kaiju_mapper/gmapper.py
@@ -1,21 +1,9 @@
 import heapq
 from dataclasses import dataclass, field
-from typing import Self
 
 import numpy as np
 from scipy.stats import anderson
 from sklearn.mixture import GaussianMixture
-
-
-def ad_test(data: np.ndarray) -> float:
-    n = len(data)
-
-    if n == 0:
-        return 0
-
-    result = anderson(data)
-
-    return result.statistic * (1 + (4 / n) - (5 / n) ** 2)  # type: ignore
 
 
 @dataclass(order=True, slots=True)
@@ -25,68 +13,132 @@ class Interval:
     lower_bound: float = field(compare=False)
     upper_bound: float = field(compare=False)
 
-    def split(self, data: np.ndarray, g_overlap: float) -> list[Self]:
-        masked_data = data[self.members]
-        mean = np.mean(masked_data)
-        std = np.std(masked_data, ddof=1)
-        m = np.sqrt(2 / np.pi) * std
-        c1, c2 = mean + m, mean - m
+
+@dataclass
+class GMapperCoverScheme:
+    iterations: int
+    max_intervals: int
+    ad_threshold: float
+    g_overlap: float
+    intervals: list[Interval] = field(default_factory=list, init=False)
+
+    def __post_init__(self):
+        if self.iterations < 1:
+            raise ValueError(f"iterations must be > 0, got {self.iterations}")
+
+        if self.max_intervals < 1:
+            raise ValueError(f"max_intervals must be > 0, got {self.max_intervals}")
+
+        if self.ad_threshold <= 0:
+            raise ValueError(f"ad_threshold must be > 0, got {self.ad_threshold}")
+
+        if self.g_overlap <= 0 or self.g_overlap >= 1:
+            raise ValueError(
+                f"g_overlap must be in the range (0,1), got {self.g_overlap}"
+            )
+
+    def __call__(self, data: np.ndarray):
+        if data.squeeze().ndim > 1:
+            raise ValueError(
+                f"Data must be one dimensional, got matrix with shape {data.shape}"
+            )
+        self.intervals = bfs(
+            lens=data.flatten(),
+            iterations=self.iterations,
+            max_intervals=self.max_intervals,
+            ad_threshold=self.ad_threshold,
+            g_overlap=self.g_overlap,
+        )
+
+        return [interval.members for interval in self.intervals]
+
+
+def ad_test(data: np.ndarray) -> float:
+    n = len(data)
+
+    if n < 8:
+        # The sample size is too small for the ad_test to be meaningful
+        return 0
+
+    result = anderson(data)
+
+    return result.statistic * (1 + (4 / n) - (5 / n) ** 2)  # type: ignore
+
+
+def split(interval: Interval, data: np.ndarray, g_overlap: float) -> list[Interval]:
+    masked_data = data[interval.members]
+    mean = np.mean(masked_data)
+    std = np.std(masked_data, ddof=1)
+    m = np.sqrt(2 / np.pi) * std
+    c1, c2 = mean + m, mean - m
+
+    try:
         gmm = GaussianMixture(
             n_components=2,
             means_init=[[c1], [c2]],
             covariance_type="full",
         ).fit(masked_data.reshape(-1, 1))
+    except ValueError:
+        # sometimes this method ends up crashing due to intermediate infs and
+        # nans. I am not sure what about the input causes this to happen so I
+        # don't know how to guard against it
+        return []
 
-        means: np.ndarray = gmm.means_.flatten()  # type: ignore
-        covariances: np.ndarray = gmm.covariances_.flatten()  # type: ignore
+    if gmm.n_features_in_ == 1:  # type: ignore
+        # Gaussian Mixture is convinced there is one cluster, we wont split here
+        return []
 
-        index = np.argsort(means)
-        means = means[index]
-        left_mean, right_mean = means[index]
-        left_std, right_std = np.sqrt(covariances[index])
+    means: np.ndarray = gmm.means_.flatten()  # type: ignore
+    covariances: np.ndarray = gmm.covariances_.flatten()  # type: ignore
 
-        new_upper_bound = left_mean + (1 + g_overlap) * left_std / (
-            left_std + right_std
-        ) * (right_mean - left_mean)
+    index = np.argsort(means)
+    means = means[index]
+    left_mean, right_mean = means[index]
+    left_std, right_std = np.sqrt(covariances[index])
 
-        new_lower_bound = right_mean - (1 + g_overlap) * right_std / (
-            left_std + right_std
-        ) * (right_mean - left_mean)
+    new_upper_bound = left_mean + (1 + g_overlap) * left_std / (
+        left_std + right_std
+    ) * (right_mean - left_mean)
 
-        result = []
+    new_lower_bound = right_mean - (1 + g_overlap) * right_std / (
+        left_std + right_std
+    ) * (right_mean - left_mean)
 
-        if new_upper_bound < self.upper_bound:
-            result.append(
-                make_interval(
-                    data, self.lower_bound, new_upper_bound, members=self.members
-                )
+    result = []
+
+    if new_upper_bound < interval.upper_bound:
+        result.append(
+            make_interval(
+                data, interval.lower_bound, new_upper_bound, mask=interval.members
             )
+        )
 
-        if new_lower_bound > self.lower_bound:
-            result.append(
-                make_interval(
-                    data, new_lower_bound, self.upper_bound, members=self.members
-                )
+    if new_lower_bound > interval.lower_bound:
+        result.append(
+            make_interval(
+                data, new_lower_bound, interval.upper_bound, mask=interval.members
             )
+        )
 
-        return result
+    return result
 
 
 def make_interval(
     data: np.ndarray,
     lower_bound: float,
     upper_bound: float,
-    members: np.ndarray | None = None,
+    mask: np.ndarray | None = None,
 ) -> Interval:
-    if members is None:
-        members = np.arange(len(data))
+    if mask is None:
+        mask = np.arange(len(data))
 
-    masked_data = data[members]
-    mask = (masked_data >= lower_bound) & (masked_data <= upper_bound)
-    members = members[mask]
+    masked_data = data[mask]
+    new_mask = np.flatnonzero(
+        (masked_data >= lower_bound) & (masked_data <= upper_bound)
+    )
     return Interval(
-        ad_score=-ad_test(data[members]),
-        members=members,
+        ad_score=-ad_test(data[new_mask]),
+        members=mask[new_mask],
         lower_bound=lower_bound,
         upper_bound=upper_bound,
     )
@@ -94,11 +146,11 @@ def make_interval(
 
 def bfs(
     lens: np.ndarray,
-    iterations: int = 10,
-    max_intervals: int = 20,
-    ad_threshold: float = 10,
-    g_overlap=0.1,
-) -> list[np.ndarray] :
+    iterations: int,
+    max_intervals: int,
+    ad_threshold: float,
+    g_overlap: float,
+) -> list[Interval]:
     cover: list[Interval] = []
     heapq.heappush(cover, make_interval(lens, np.min(lens), np.max(lens)))
     iteration = 0
@@ -117,18 +169,17 @@ def bfs(
 
         iteration += 1
         interval = heapq.heappop(cover)
-        new_intervals = interval.split(lens, g_overlap)
 
-        print(f"{len(new_intervals)=}")
+        new_intervals = split(interval, lens, g_overlap)
 
         if len(new_intervals) == 0:
             # splitting the interval resulted in no new intervals, to stop us
             # checking again we put it back on the heap with an impossibly
-            # large ad_score
+            # small ad_score
             interval.ad_score = np.inf
             heapq.heappush(cover, interval)
 
         for interval in new_intervals:
             heapq.heappush(cover, interval)
 
-    return [interval.members for interval in cover]
+    return cover
