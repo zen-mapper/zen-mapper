8c57726762df9c9d253e298b6c49d41de997d2a4
Ethan Rooke <ethan@roo.ke>
2025-06-14 16:17:32 -0500

    kaiju: re-export zen mapper stuff

---
+++ packages/kaiju-mapper/src/kaiju_mapper/__init__.py
@@ -0,0 +1,5 @@
+from zen_mapper import mapper
+
+from kaiju_mapper.gmapper import GMapperCoverScheme
+
+__all__ = ("GMapperCoverScheme", "mapper")
---
+++ packages/kaiju-mapper/src/kaiju_mapper/adapters.py
@@ -0,0 +1,3 @@
+from zen_mapper.adapters import sk_learn, to_networkx
+
+__all__ = ("sk_learn", "to_networkx")
