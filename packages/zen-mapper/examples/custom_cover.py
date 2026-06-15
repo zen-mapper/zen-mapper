725a1fc49d7b68e0255d9926ad39a2c922249eeb
Ethan Rooke <ethan@roo.ke>
2026-06-15 18:37:25 -0500

    docs: update custom_cover example to new cover api

--- packages/zen-mapper/examples/custom_cover.py
+++ packages/zen-mapper/examples/custom_cover.py
@@ -9,109 +9,145 @@ actual implementation of the cover itself and there may be better ways.
 """
 
 # %%
-# Constructing a cover
+# Representing a cover
 # ====================
 #
-# We will start by defining an epsilon-net function. This will take a list of
-# centers, an epsilon, and a data array and return a list of numpy arrays.
+# Zen Mapper represents sets as numpy arrays of indices into a data array. That
+# is to say `np.array([1, 2, 5])` represents the set with elements `1`, `2`,
+# and `5` from the original, high dimensional data. Zen mapper then represents
+# a cover as an :term:`Iterable <python:iterable>` of these cover elements. If you're not
+# comfortable with what an :term:`Iterable <python:iterable>` is, you can think of them as a
+# :class:`list` for now. To construct a cover that zen mapper understands will
+# require us to adhere to this format.
+
+# %%
+# Constructing an epsilon net
+# ===========================
+#
+# An epsilon-net is a collection of balls of radius epsilon. The covers used in
+# ball mapper are all variations on generating an epsilon-net. So to start we
+# will focus on creating a function which takes a description of an epsilon-net
+# along with some data and constructs a cover as outlined in the previous
+# section.
 
 import numpy as np
-
-import zen_mapper as zm
 
 
 def epsilon_net(centers, epsilon, data) -> list[np.ndarray]:
     if len(data.shape) == 1:
         data.reshape(-1, 1)
 
-    result = []
+    cover = []
     for center in centers:
-        current = []
-        for i, datum in enumerate(data):
-            if np.sum((center - datum) ** 2) < epsilon**2:
-                current.append(i)
-        result.append(np.array(current, dtype=int))
+        # Create a new cover element for each center point
+        cover_element = []
 
-    return result
+        for i, datum in enumerate(data):
+            # Check if the distance between the center point
+            # and the data point is less than epsilon
+            if np.sum((center - datum) ** 2) < epsilon**2:
+                # If so append it to our cover element
+                cover_element.append(i)
+
+        # Finally add our cover element to the result
+        cover.append(np.array(cover_element, dtype=int))
+
+    return cover
 
 
 # %%
-# It is worth noting that :class:`list[np.ndarray]` implements the
-# :class:`Cover <zen_mapper.types.Cover>` protocol and so `epsilon_net` is
-# returning a valid `zen_mapper` cover. We can then visualize that this cover
-# acts as we hope it would
-
+# It is worth noting that :class:`list`\[:class:`numpy.ndarray`] is an :term:`Iterable
+# <python:iterable>` and so is understood by zen mapper. We can then visualize
+# that this cover acts as we hope it would.
 import matplotlib.pyplot as plt
 
+# We construct a diagonal dataset
 data = np.c_[np.arange(11), np.arange(11)]
 centers = np.array([[5, 5], [3, 3]])
 epsilon = 2
 
 ax = plt.gca()
+
+# Plot the dataset
 ax.scatter(data[:, 0], data[:, 1])
+
+# Compute the cover
 cover = epsilon_net(centers, epsilon, data)
 
-for element in cover:
-    ax.scatter(data[element, 0], data[element, 1])
+# Plot each element of that cover
+for i, element in enumerate(cover):
+    ax.scatter(
+        data[element, 0],
+        data[element, 1],
+        label=f"Cover Element {i}",
+    )
 
+# Visualize each epsilon ball
 for center in centers:
     circ = plt.Circle(center, epsilon, color="r", fill=False)
     ax.add_patch(circ)
 
 ax.set_aspect("equal")
+ax.legend()
 plt.show()
 
+
 # %%
-# Creating a covering scheme
-# ==========================
+# A greedy epsilon net
+# ====================
 #
-# The mapper function in zen_mapper expects a `covering_scheme` which is simply
-# a function which takes data and returns a cover. I find pythons partial
-# function support to be kind of awkward. So instead of defining a function we
-# will define a class with the `__call__` method which python will treat like
-# a function.
+# Ball mapper goes on to define a "greedy" epsilon net. The user specifies a
+# dataset and an epsilon value and from there the algorithm proceeds to
+# repeatedly choose a point not currently covered by the epsilon net and adds
+# this point as a center point to the net. We will define a function which
+# implements this algorithm.
 
 
-class Greedy_Epsilon_Net:
-    def __init__(self, epsilon: float):
-        self.epsilon = epsilon
-        self.centers = None
+def greedy_epsilon_net(epsilon: float, data: np.ndarray):
+    # These are the points which are not covered
+    to_cover = set(range(len(data)))
 
-    def __call__(self, data: np.ndarray) -> list[np.ndarray]:
-        to_cover = set(range(len(data)))
+    centers = []
 
-        centers = []
+    # If there are still points to cover
+    while to_cover:
+        # Pick the first one in the set
+        new_center = to_cover.pop()
 
-        while to_cover:
-            new_center = to_cover.pop()
-            centers.append(data[new_center])
-            covered = set()
+        # Add it to the list of centers
+        centers.append(data[new_center])
+        covered = set()
 
-            for point in to_cover:
-                if np.sum((data[new_center] - data[point]) ** 2) < self.epsilon**2:
-                    covered.add(point)
+        for point in to_cover:
+            # Mark the point as covered
+            if np.sum((data[new_center] - data[point]) ** 2) < epsilon**2:
+                covered.add(point)
 
-            to_cover -= covered
+        # Remove all the points we covered from the set to cover
+        to_cover -= covered
 
-        self.centers = centers
+    # Construct the cover from these center points
+    cover = epsilon_net(centers, epsilon, data)
 
-        return epsilon_net(centers, self.epsilon, data)
+    # Return the cover along with the centers
+    return cover, centers
 
 
 # %%
-# we can then visualize the covering scheme in much the same way we visualized the cover
+# By returning the center points along with the cover we allow users to
+# introspect how the cover was constructed. We can then visualize the covering
+# scheme in much the same way we visualized the cover.
 
 data = np.c_[np.arange(11), np.arange(11)]
-scheme = Greedy_Epsilon_Net(epsilon=2)
+cover, centers = greedy_epsilon_net(epsilon=2, data=data)
 
 ax = plt.gca()
 ax.scatter(data[:, 0], data[:, 1])
-cover = scheme(data)
 
 for element in cover:
     ax.scatter(data[element, 0], data[element, 1])
 
-for center in scheme.centers:
+for center in centers:
     circ = plt.Circle(center, epsilon, color="r", fill=False)
     ax.add_patch(circ)
 
@@ -142,9 +178,31 @@ plt.gca().set_aspect("equal")
 plt.show()
 
 # %%
-# We also need to define a clustering algorithm. In this paper they don't
+# We then can fit our cover to this data set, visualizing it as we did in
+# previous examples
+
+epsilon = 1.25
+cover, centers = greedy_epsilon_net(data=data, epsilon=epsilon)
+
+ax = plt.gca()
+ax.scatter(data[:, 0], data[:, 1])
+
+for element in cover:
+    ax.scatter(data[element, 0], data[element, 1])
+
+for center in centers:
+    circ = plt.Circle(center, epsilon, color="r", fill=False)
+    ax.add_patch(circ)
+
+ax.set_aspect("equal")
+plt.show()
+
+# %%
+# Finally we need to define a clustering algorithm. In this paper they don't
 # actually cluster anything, instead any datapoint in the ball is deemed to be
-# connected. So we define the following trivial clusterer.
+# connected. So we define the following trivial clusterer. See
+# :ref:`sphx_glr_examples_custom_clusterer.py` for more  information on how to
+# construct a clusterer for zen mapper.
 
 
 def trivial(data: np.ndarray, elements: np.ndarray):
@@ -157,10 +215,11 @@ def trivial(data: np.ndarray, elements: np.ndarray):
 
 import networkx as nx
 
+import zen_mapper as zm
+
 result = zm.mapper(
     data=data,
-    projection=data,
-    cover_scheme=Greedy_Epsilon_Net(1),
+    cover=cover,
     clusterer=trivial,
     dim=1,
 )
